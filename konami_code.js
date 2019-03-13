31b0287d788dab00da2bbf5e6ccaf74a3a986890
stomg7969 <stomg7969@gmail.com>
2019-03-12 23:06:48 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,20 @@ const codes = [
 
 function init() {
   // your code here
+  let i = 0;
+  document.body.addEventListener('keydown', (ev) => {
+    console.log(ev.key);
+    if (ev.key === codes[i]) {
+      i++;
+      console.log(i);
+
+      if (i === codes.length) {
+        alert("nice");
+        console.log('nice')
+        i = 0;
+      }
+    } else {
+      i = 0;
+    }
+  });
 }
